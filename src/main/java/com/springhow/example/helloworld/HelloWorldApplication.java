19877ec68dd09fac2e8bf56ecb5826a0ea826b05
Rushi568 <rtrivedi68@gmail.com>
2023-07-10 20:33:45 +0530

    Changed SOP statement

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "5. please visit my website learning-ocean.com";
+        return "WELCOME TO SESSION ON DEVOPS BY RUSHI TRIVEDI DEVOPS";
     }
 
 }
